15dd040cfb7fb5f08a8cc8f878726183fb877e30
konomae <konomae@users.noreply.github.com>
2025-02-16 15:33:48 +0900

    Support proto v0.46

--- tests/metadata_test.rs
+++ tests/metadata_test.rs
@@ -5,7 +5,7 @@ async fn registers_metadata() {
     let sandbox = create_empty_proto_sandbox();
     let plugin = sandbox.create_plugin("zls-test").await;
 
-    let metadata = plugin.register_tool(ToolMetadataInput::default()).await;
+    let metadata = plugin.register_tool(RegisterToolInput::default()).await;
 
     assert_eq!(metadata.name, "ZLS");
     assert_eq!(metadata.type_of, PluginType::CommandLine);
